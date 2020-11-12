f3d34523a42693fc253efa72fc2d1caa85a0a2d5
JosephBaquerizo <61207703+JosephBaquerizo@users.noreply.github.com>
2020-11-12 01:29:03 -0500

    Agregando Loading component

--- pokecheckApp/App.js
+++ pokecheckApp/App.js
@@ -5,21 +5,36 @@ import MainView from "./app/screens/MainView";
 import TopBar from "./app/components/TopBar";
 import PokemonContainer from "./app/components/PokemonContainer";
 import BottomBar from "./app/components/BottomBar";
+import Loading from "./app/components/Loading";
 
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      pokemones: []
+      pokemones: [],
+      loading: false
     }
   }
 
   async componentDidMount() {
+    this.setState({loading: true});
+    //for (let i = 1; i < 152; i++) {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
     const json = await response.json();
-    this.setState({pokemones: json.name});
+    const info = {
+      name: json.name,
+      id: json.id,
+      image: json.sprites.front_default,
+      typeList: json.types[0].type.name,
+      height: json.height,
+      weight: json.weight
+    }
+    //}
+    this.setState({loading: false});
+    this.setState({pokemones: info});
     console.log(this.state.pokemones);
+    //console.log(this.state.loading);
   }
 
   render () {
@@ -28,12 +43,13 @@ class App extends Component {
         <TopBar/>
         <PokemonContainer/>
         <BottomBar/>
+        <Loading isVisible={this.state.loading} text="Cargando..."/>
       </View>
     );
   }
 }
 
-
+//<Loading isVisible={this.loading} text="Cargando..."/>
 
 const styles = StyleSheet.create({
   container: {
